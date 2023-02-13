26f1d9a0f627c2bb4beb811d1388198588d098eb
John Buchanan <john@buchanandevops.com>
2023-02-13 09:56:21 -0500

    app, document, geist installed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
+import { GeistProvider, CssBaseline } from '@geist-ui/core'
+import { Inter } from "@next/font/google"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const inter = Inter({ subsets: ["latin"] })
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component className={inter.className} {...pageProps} />
+    </GeistProvider>
+  ) 
 }
+
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+})
+
